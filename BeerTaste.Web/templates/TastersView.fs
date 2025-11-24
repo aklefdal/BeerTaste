e25fe64d9f042ca48bce812119128de9f3e1e73b
Alf Kåre Lefdal <alf.kare@lefdal.cc>
2025-11-24 01:28:52 +0100

    Copilot: Mask email

--- BeerTaste.Web/templates/TastersView.fs
+++ BeerTaste.Web/templates/TastersView.fs
@@ -4,6 +4,20 @@ open Oxpecker.ViewEngine
 open BeerTaste.Common
 open BeerTaste.Web.Templates.Layout
 
+let maskEmail (email: string) =
+    match email.Split '@'  with
+    | [| local; domain |] ->
+        let maskedLocal =
+            if local.Length <= 2 then local + "**"
+            else local.Substring(0, 2) + "**"
+        let keepDomain =
+            if domain.Length <= 5 then domain else domain.Substring(domain.Length - 5)
+
+        $"%s{maskedLocal}@**%s{keepDomain}"
+
+    | _ -> email
+
+
 let view (beerTasteGuid: string) (tasters: Taster list) =
     layout "Tasters" beerTasteGuid [
         h1 () { raw "Tasters" }
@@ -23,7 +37,7 @@ let view (beerTasteGuid: string) (tasters: Taster list) =
                 for taster in tasters do
                     tr () {
                         td () { raw taster.Name }
-                        td () { raw (taster.Email |> Option.defaultValue "") }
+                        td () { raw (taster.Email |> Option.map maskEmail |> Option.defaultValue "") }
 
                         td (class' = "value") {
                             raw (
